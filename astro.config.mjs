1c6fc553eb77584da8657e7324c394d7a60f1a67
vedvedved <vedikajamdar@gmail.com>
2025-11-13 01:06:29 +0530

    deployment fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import solid from "@astrojs/solid-js";
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 /*
   Using adapter: node({ mode: 'standalone' })
